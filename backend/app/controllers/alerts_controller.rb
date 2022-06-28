43350fc18f5c0b1fcc2176a5429b6c883e81aeba
carolyn-manning <carolyn.manning3@gmail.com>
2022-06-27 23:47:39 -0400

    update alert controller. start testing fetch request for alerts

--- backend/app/controllers/alerts_controller.rb
+++ backend/app/controllers/alerts_controller.rb
@@ -1,2 +1,11 @@
 class AlertsController < ApplicationController
+
+    def index 
+        alerts =  Alert.all 
+        render json: alerts
+
+        # build current_user update to work with current_user
+
+    end 
+
 end
--- frontend/src/containers/alertContainer.js
+++ frontend/src/containers/alertContainer.js
@@ -4,7 +4,39 @@ import LocationInput from '../components/locationInput';
 
 class AlertContainer extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            alerts: []
+        };
+    }
+
+    fetchAlerts = () => {
+
+        const configObj = {
+            method: "GET", 
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                alerts: this.state.alerts
+                })
+        }
+
+        fetch(`http://localhost:4000/alerts/`, configObj)
+        .then(response => response.json())
+        .then(data => {
+           console.log(data)
+        })   
+    }
+
+    componentDidMount() {
+        this.fetchAlerts()
+    }
+
     render() {
+
 
         const test_alerts = [{origin: "1", destination: "1", time: "1"}, {origin: "2", destination: "2", time: "2"}]
         return (
