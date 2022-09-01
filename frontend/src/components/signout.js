d637f898f60ebe1e2b09e3ff8598e8e98c55f05b
carolyn-manning <carolyn.manning3@gmail.com>
2022-08-31 22:39:43 -0400

    reroute to login page upon logout

--- frontend/src/components/signout.js
+++ frontend/src/components/signout.js
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 export function SignOut() {
   
-  // let navigate = useNavigate()
+  let navigate = useNavigate()
 
   function handleOnClick () {
     localStorage.removeItem("jwt");
-   // navigate('/') once routing is determined
+    navigate('/login') 
   }
 
   return <div className="sign-out-container"> <button id="sign-out-button" onClick={handleOnClick}>LOG OUT</button> </div>
